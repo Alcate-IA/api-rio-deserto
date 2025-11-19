3227d18069fa20ebd89485010ef2ab89805eb44c
RafaelAugustoR <rafaelaugustodev@gmail.com>
2025-11-19 10:23:26 -0300

    First commit

---
+++ src/main/java/com/rioDesertoAcessoDb/RioDesertoAcessoBbApplication.java
@@ -0,0 +1,13 @@
+package com.rioDesertoAcessoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RioDesertoAcessoBbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RioDesertoAcessoBbApplication.class, args);
+	}
+
+}
