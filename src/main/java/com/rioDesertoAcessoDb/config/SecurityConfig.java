1f44ccd98bc1f11cd28387d71cbf96455db0df59
matheusilveiraw <matheus.silveiraw@gmail.com>
2026-01-30 16:03:46 -0300

    api ccors

--- src/main/java/com/rioDesertoAcessoDb/config/SecurityConfig.java
+++ src/main/java/com/rioDesertoAcessoDb/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
